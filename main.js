7df6dc15a0a23bcc7c02437e8e3bda7feee77731
shingo <reg@shin-go.net>
2019-12-12 23:06:17 +0900

    描画メソッドの最適化

--- main.js
+++ main.js
@@ -77,30 +77,68 @@
  }
 
  function draw(actionName, selection){
-   console.log("draw", actionName);
-   let color = "#333333";
-   let x = 10;
-   let y = 10;
-   switch (actionName) {
-    case "straight":
-       drawStraightArrow(x, y, color, selection);
-       break;
-    case "curve":
-      drawCurveArrow(x, y, color, selection);
-      break;
-    case "snake":
-     drawSnakeArrow(x, y, color, selection);
-     default:
 
-   }
+   drawLineAndEdge({
+     x: 10,
+     y: 10,
+     width: 100,
+     height: 100,
+     edgeType: "arrow2",
+     lineType: actionName,
+     lineWidth: 1,
+     color: "#333333"
+   },selection);
  }
 
- function drawStraightArrow(sx, sy, color, selection){
-   const width = 100;
-   const line = createStraightLine(sx, sy, sx + width, sy, color);
+ /*
+  drowParms = {
+    x:Number,
+    y:Number,
+    width:Number,
+    height:Number,
+    edgeType:"none" or "arrow1" or "arrow2" or "arrow3" or "circle1" or "circle2" or "square1" or "square2",
+    lineType:"straight" or "curve" or "snake",
+    lineWidth:Number,
+    color:String
+  }
+ */
+
+ function drawLineAndEdge(parms, selection){
+
+   const x = parms.x;
+   const y = parms.y;
+   const width = parms.width;
+   const height = parms.height;
+   const edgeType = parms.edgeType;
+   const lineType = parms.lineType;
+   const lineWidth = parms.lineWidth;
+   const color = parms.color;
+
+   const line = createLine({
+     sx: x,
+     sy: y,
+     ex: x + width,
+     ey: y + height,
+     type: lineType,
+     lineWidth: lineWidth,
+     color: color
+   });
    selection.insertionParent.addChild(line);
 
-   const edge = createArrowEdge(sx + width, sy, true, color);
+   let edgeX = x + width;
+   let edgeY = y;
+   if(lineType == "curve" || lineType == "snake"){
+     edgeY = y + height;
+   }
+
+
+   const edge = createEdge({
+     x: edgeX,
+     y: edgeY,
+     type: edgeType,
+     lineWidth: lineWidth,
+     color: color
+   }, true);
    selection.insertionParent.addChild(edge);
 
    selection.items = [line, edge];
@@ -109,94 +147,113 @@
    group.name = "StraightArrow";
  }
 
- function drawCurveArrow(sx, sy, color, selection){
-   const width = 100;
-   const height = 100;
-   const line = createCurveLine(sx, sy, sx + width, sy + height, color);
-   selection.insertionParent.addChild(line);
 
-   const edge = createArrowEdge(sx + width, sy + height, true, color);
-   selection.insertionParent.addChild(edge);
+ function createLine(parms){
+   console.log("createLine", parms);
+   const sx = parms.sx;
+   const sy = parms.sy;
+   const ex = parms.ex;
+   const ey = parms.ey;
+   const type = parms.type;
+   const lineWidth = parms.lineWidth;
+   const color = parms.color;
 
-   selection.items = [line, edge];
-   commands.group();
-   let group = selection.items[0];
-   group.name = "CurveArrow";
- }
-
- function drawSnakeArrow(sx, sy, color, selection){
-   const width = 100;
-   const height = 100;
-   const line = createSnakeLine(sx, sy, sx + width, sy + height, color);
-   selection.insertionParent.addChild(line);
-
-   const edge = createArrowEdge(sx + width, sy + height, true, color);
-   selection.insertionParent.addChild(edge);
-
-   selection.items = [line, edge];
-   commands.group();
-   let group = selection.items[0];
-   group.name = "CurveArrow";
- }
-
-
-
- function createStraightLine(sx, sy, ex, ey, color){
-
-   const startPoint = sx + "," + sy;
-   const endPoint = ex + "," + ey;
-   const pathData = `M ${startPoint} L ${startPoint} ${endPoint}`;
-   const path = new Path();
-   path.stroke = new Color(color);
-   path.pathData = pathData;
-   return path;
- }
-
- function createCurveLine(sx, sy, ex, ey, color){
-
-   const startPoint = sx + "," + sy;
-   const curvePoint = sx + "," + ey;
-   const endPoint = ex + "," + ey;
-   const pathData = `M ${startPoint} L ${startPoint} ${curvePoint} ${endPoint}`;
-   const path = new Path();
-   path.stroke = new Color(color);
-   path.pathData = pathData;
-   return path;
- }
-
- function createSnakeLine(sx, sy, ex, ey, color){
-   const mx = (ex - sx)/2 + sx;
-   const startPoint = sx + "," + sy;
-   const curvePoint1 = mx + "," + sy;
-   const curvePoint2 = mx + "," + ey;
-   const endPoint = ex + "," + ey;
-   const pathData = `M ${startPoint} L ${startPoint} ${curvePoint1} ${curvePoint2} ${endPoint}`;
-   const path = new Path();
-   path.stroke = new Color(color);
-   path.pathData = pathData;
-   return path;
- }
-
- function createArrowEdge(x, y, toRight, color){
-   let p1, p2, p3;
-
-   if(toRight){
-     p1 = (x-10) + "," + (y-10);
-     p2 = x + "," + y;
-     p3 = (x-10) + "," + (y+10);
-   }else{
-     p1 = (x+10) + "," + (y-10);
-     p2 = x + "," + y;
-     p3 = (x+10) + "," + (y+10);
+   let p0, p1, p2, p3;
+   let pathData = "";
+   switch(type){
+     case "straight":
+      p0 = sx + "," + sy;
+      p1 = ex + "," + sy;
+      pathData = `M ${p0} L ${p0} ${p1}`;
+     break;
+     case "curve":
+      p0 = sx + "," + sy;
+      p1 = sx + "," + ey;
+      p2 = ex + "," + ey;
+      pathData = `M ${p0} L ${p0} ${p1} ${p2}`;
+     break;
+     case "snake":
+      const mx = (ex - sx)/2 + sx;
+      p0 = sx + "," + sy;
+      p1 = mx + "," + sy;
+      p2 = mx + "," + ey;
+      p3 = ex + "," + ey;
+      pathData = `M ${p0} L ${p0} ${p1} ${p2} ${p3}`;
+     break;
    }
 
-   const pathData = `M ${p1} L ${p1} ${p2} ${p3}`;
    const path = new Path();
    path.stroke = new Color(color);
+   path.strokeWidth = lineWidth;
    path.pathData = pathData;
+   path.name = "line";
    return path;
-
  }
+
+
+
+
+ function createEdge(parms, toRight){
+
+   console.log("createEdge", parms);
+   const x = parms.x;
+   const y = parms.y;
+   const type = parms.type;
+   const lineWidth = parms.lineWidth;
+   const color = parms.color;
+
+   let p0,p1,p2;
+   let pathData = "";
+   let stroke;
+   let fill;
+
+   switch(type){
+     case "arrow1":
+
+      if(toRight){
+        p0 = (x-15) + "," + (y-10);
+        p1 = x + "," + y;
+        p2 = (x-15) + "," + (y+10);
+      }else{
+        p0 = (x+15) + "," + (y-10);
+        p1 = x + "," + y;
+        p2 = (x+15) + "," + (y+10);
+      }
+      pathData = `M ${p0} L ${p0} ${p1} ${p2}`;
+      stroke = new Color(color);
+
+     break;
+     case "arrow2":
+
+       if(toRight){
+         p0 = (x-15) + "," + (y-10);
+         p1 = x + "," + y;
+         p2 = (x-15) + "," + (y+10);
+       }else{
+         p0 = (x+15) + "," + (y-10);
+         p1 = x + "," + y;
+         p2 = (x+15) + "," + (y+10);
+       }
+       pathData = `M ${p0} L ${p0} ${p1} ${p2} ${p0}`;
+       stroke = new Color(color);
+       fill = new Color(color);
+
+     break;
+   }
+
+   const path = new Path();
+   path.stroke = stroke;
+   path.fill = fill;
+   path.strokeWidth = lineWidth;
+   path.pathData = pathData;
+   if(toRight){
+     path.name = "rightEdge";
+   }else{
+     path.name = "leftEdge";
+   }
+   return path;
+ }
+
 
  module.exports = {
      panels: {
