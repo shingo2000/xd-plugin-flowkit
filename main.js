c05f49848f2c33e7245678b64754344646e8d6e9
shingo <reg@shin-go.net>
2019-12-15 07:01:36 +0900

    panelControllerを分離

--- main.js
+++ main.js
@@ -8,8 +8,8 @@
  const { editDocument } = require("application");
  const commands = require("commands");
  const sceneController = require("./sceneController.js");
+ const panelController = require("./panelController.js");
 
- let panel;
  const settingData = {
    leftEdgeType: "none",
    rightEdgeType: "arrow3",
@@ -18,193 +18,22 @@
    lineType: "straignt"
  }
 
- function create() {
-   console.log("create");
-   const html = `
-<style>
-  .actionButton {
-    width: 100%;
-    background: #fff;
-    padding: 10px;
-    border-radius: 4px;
-    display: block;
-    border: 1px solid #fff;
-    margin-bottom: 4px;
-  }
-  .actionButton:hover{
-    border: 1px solid #999;
-  }
-  .actionButton span{
-    font-size: 108%;
-    line-height: 24px;
-    display: inline-block;
-    color: #333;
-    text-decoration: none;
-  }
-  .actionButton img{
-    width: 24px; height: 24px;
-    margin-right: 16px;
-  }
-  .hide {
-    display: none;
-  }
-  .show{
-    display: block;
-  }
-  .row { align-items: center; }
-  div{
-    width: 100%;
-  }
-  .spread {
-        justify-content: space-between;
-        width: 100%;
-  }
-  input[type="range"]{
-    width: 100%;
-  }
-  .inlineBlock{
-    display: inlineBlock;
-  }
-
-</style>
-<div id="toolPanel" class="show">
-  <a href="#" data-action="straight" class="actionButton">
-    <img src="assets/icon_straight.png" />
-    <span>直線</span>
-  </a>
-  <a href="#" data-action="curve" class="actionButton">
-    <img src="assets/icon_curve.png" />
-    <span>折れ線</span>
-  </a>
-  <a href="#" data-action="snake" class="actionButton">
-    <img src="assets/icon_snake.png" />
-    <span>カギ線</span>
-  </a>
-</div>
-<div id="propertyPanel" class="hide">
-  <label>
-    <div class="row spread">
-        <span>線の太さ</span>
-        <span>1</span>
-    </div>
-    <input type="range" id="lineWidth" min=0.5 max=5 value=1 step=0.5 />
-  </label>
-  <div>
-    <label class="inlineBlock">
-      <span>左端</span>
-      <select id="leftEdge">
-           <option value="none">なし</option>
-           <option value="arrow1">矢印１</option>
-           <option value="arrow2">矢印２</option>
-           <option value="arrow3">矢印３</option>
-           <option value="circle1">円形１</option>
-           <option value="circle2">円形２</option>
-           <option value="square1">四角１</option>
-           <option value="square2">四角２</option>
-      </select>
-    </label>
-    <label class="inlineBlock">
-      <span>右端</span>
-      <select id="rightEdge">
-           <option value="none">なし</option>
-           <option value="arrow1">矢印１</option>
-           <option value="arrow2">矢印２</option>
-           <option value="arrow3">矢印３</option>
-           <option value="circle1">円形１</option>
-           <option value="circle2">円形２</option>
-           <option value="square1">四角１</option>
-           <option value="square2">四角２</option>
-      </select>
-    </label>
-  </div>
-  <label>
-    <div class="row spread">
-        <span>ポイントの大きさ</span>
-        <span>100%</span>
-    </div>
-    <input type="range" id="edgeScale" min=10 max=500 value=100 step=20 />
-  </label>
-  <input type="hidden" id="lineType" value="straight" />
-  <input type="hidden" id="color" value="#cc0000" />
-</div>
-  `;
-  function onActionButton(e){
-    let actionName = e.currentTarget.getAttribute('data-action');
-    editDocument({ editLabel: "draw" }, function(selection) {
-      draw(actionName, selection);
-    });
-  }
-
-  function onChangeProperty(e){
-    console.log("onChangeProperty");
-    const lineWidth = document.querySelector("#lineWidth").value;
-    const leftEdgeType = document.querySelector("#leftEdge").value;
-    const rightEdgeType = document.querySelector("#rightEdge").value;
-    const edgeScale = document.querySelector("#edgeScale").value;
-    const lineType = document.querySelector("#lineType").value;
-    const color = document.querySelector("#color").value;
-    console.log(lineWidth, leftEdgeType, rightEdgeType, edgeScale);
-
-    editDocument({ editLabel: "redraw" }, function(selection) {
-      redraw({
-        lineWidth: lineWidth,
-        leftEdgeType: leftEdgeType,
-        rightEdgeType: rightEdgeType,
-        lineType: lineType,
-        color: color
-
-      }, selection);
-    });
-  }
-
-  panel = document.createElement("div");
-  panel.innerHTML = html;
-
-  const buttons = panel.querySelectorAll(".actionButton");
-  for(let i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener("click", onActionButton);
-  }
-
-  const inputs = panel.querySelectorAll("#propertyPanel input, #propertyPanel select");
-  for(let i = 0; i < inputs.length; i++){
-    inputs[i].addEventListener("change", onChangeProperty);
-  }
-
-  return panel;
- }
-
  function show(event) {
-   if (!panel) event.node.appendChild(create());
+   function onActionButton(actionName){
+     editDocument({ editLabel: "draw" }, function(selection) {
+       draw(actionName, selection);
+     });
+   }
+   function onChangeProperty(prms){
+     editDocument({ editLabel: "redraw" }, function(selection) {
+       redraw(prms, selection);
+     });
+   }
+   panelController.show(event,onActionButton,onChangeProperty);
  }
 
  function update(selection) {
-   const toolPanel = document.querySelector("#toolPanel");
-   const propertyPanel = document.querySelector("#propertyPanel");
-   if(selection.items[0] && selection.items[0].pluginData &&selection.items[0].pluginData.name == "flowKitConnector"){
-     updateToolPanel(selection.items[0].pluginData);
-     toolPanel.className = "hide";
-     propertyPanel.className = "show";
-   }else{
-     toolPanel.className = "show";
-     propertyPanel.className = "hide";
-   }
- }
- /*
- function resetToolPanel(){
-   panel.querySelector("#propertyPanel #lineWidth").value = settingData.lineWidth;
-   panel.querySelector("#propertyPanel #leftEdge").value = settingData.leftEdgeType;
-   panel.querySelector("#propertyPanel #rightEdge").value = settingdata.rightEdgeType;
-   panel.querySelector("#propertyPanel #lineType").value = settingdata.lineType;
-   panel.querySelector("#propertyPanel #color").value = settingdata.color;
- }*/
- function updateToolPanel(parms){
-   console.log("updateToolPanel",parms)
-   panel.querySelector("#propertyPanel #lineWidth").value = parms.lineWidth;
-   panel.querySelector("#propertyPanel #leftEdge").value = parms.leftEdgeType;
-   panel.querySelector("#propertyPanel #rightEdge").value = parms.rightEdgeType;
-   panel.querySelector("#propertyPanel #lineType").value = parms.lineType;
-   panel.querySelector("#propertyPanel #color").value = parms.color;
-
+   panelController.update(selection);
  }
 
  function redraw(parms,selection){
---
+++ panelController.js
@@ -0,0 +1,174 @@
+
+const { editDocument } = require("application");
+
+let panel;
+
+function create(onActionButton, onChangeProperty) {
+  const html = `
+<style>
+ .actionButton {
+   width: 100%;
+  background: #fff;
+   padding: 10px;
+   border-radius: 4px;
+   display: block;
+   border: 1px solid #fff;
+   margin-bottom: 4px;
+ }
+ .actionButton:hover{
+   border: 1px solid #999;
+ }
+ .actionButton span{
+   font-size: 108%;
+   line-height: 24px;
+   display: inline-block;
+   color: #333;
+   text-decoration: none;
+ }
+ .actionButton img{
+   width: 24px; height: 24px;
+   margin-right: 16px;
+ }
+ .hide {
+   display: none;
+ }
+ .show{
+   display: block;
+ }
+ .row { align-items: center; }
+ div{
+   width: 100%;
+ }
+ .spread {
+       justify-content: space-between;
+       width: 100%;
+ }
+ input[type="range"]{
+   width: 100%;
+ }
+ .inlineBlock{
+   display: inlineBlock;
+ }
+
+</style>
+<div id="toolPanel" class="show">
+ <a href="#" data-action="straight" class="actionButton">
+   <img src="assets/icon_straight.png" />
+   <span>直線</span>
+ </a>
+ <a href="#" data-action="curve" class="actionButton">
+   <img src="assets/icon_curve.png" />
+   <span>折れ線</span>
+ </a>
+ <a href="#" data-action="snake" class="actionButton">
+   <img src="assets/icon_snake.png" />
+   <span>カギ線</span>
+ </a>
+</div>
+<div id="propertyPanel" class="hide">
+ <label>
+   <div class="row spread">
+       <span>線の太さ</span>
+       <span>1</span>
+   </div>
+   <input type="range" id="lineWidth" min=0.5 max=5 value=1 step=0.5 />
+ </label>
+ <div>
+   <label class="inlineBlock">
+     <span>左端</span>
+     <select id="leftEdge">
+          <option value="none">なし</option>
+          <option value="arrow1">矢印１</option>
+          <option value="arrow2">矢印２</option>
+          <option value="arrow3">矢印３</option>
+          <option value="circle1">円形１</option>
+          <option value="circle2">円形２</option>
+          <option value="square1">四角１</option>
+          <option value="square2">四角２</option>
+     </select>
+   </label>
+   <label class="inlineBlock">
+     <span>右端</span>
+     <select id="rightEdge">
+          <option value="none">なし</option>
+          <option value="arrow1">矢印１</option>
+          <option value="arrow2">矢印２</option>
+          <option value="arrow3">矢印３</option>
+          <option value="circle1">円形１</option>
+          <option value="circle2">円形２</option>
+          <option value="square1">四角１</option>
+          <option value="square2">四角２</option>
+     </select>
+   </label>
+ </div>
+ <label>
+   <div class="row spread">
+       <span>ポイントの大きさ</span>
+       <span>100%</span>
+   </div>
+   <input type="range" id="edgeScale" min=10 max=500 value=100 step=20 />
+ </label>
+ <input type="hidden" id="lineType" value="straight" />
+ <input type="hidden" id="color" value="#cc0000" />
+</div>
+ `;
+  panel = document.createElement("div");
+  panel.innerHTML = html;
+  const buttons = panel.querySelectorAll(".actionButton");
+  for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", _onActionButton);
+  }
+  const inputs = panel.querySelectorAll("#propertyPanel input, #propertyPanel select");
+  for(let i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener("change", _onChangeProperty);
+  }
+  function _onChangeProperty(e){
+    onChangeProperty({
+      lineWidth: document.querySelector("#lineWidth").value,
+      leftEdgeType: document.querySelector("#leftEdge").value,
+      rightEdgeType: document.querySelector("#rightEdge").value,
+      edgeScale: document.querySelector("#edgeScale").value,
+      lineType: document.querySelector("#lineType").value,
+      color: document.querySelector("#color").value
+    });
+  }
+  function _onActionButton(e){
+    let actionName = e.currentTarget.getAttribute('data-action');
+    onActionButton(actionName);
+  }
+  return panel;
+}
+
+function show(event, onActionButton, onChangeProperty) {
+  if (!panel) event.node.appendChild(create(onActionButton, onChangeProperty));
+}
+
+
+function update(selection) {
+//console.log("update",panel);
+ const toolPanel = panel.querySelector("#toolPanel");
+ const propertyPanel = panel.querySelector("#propertyPanel");
+ if(selection.items[0] && selection.items[0].pluginData &&selection.items[0].pluginData.name == "flowKitConnector"){
+   updateToolPanel(selection.items[0].pluginData);
+   toolPanel.className = "hide";
+   propertyPanel.className = "show";
+ }else{
+   toolPanel.className = "show";
+   propertyPanel.className = "hide";
+ }
+}
+
+
+function updateToolPanel(parms){
+  //console.log("updateToolPanel",parms)
+  panel.querySelector("#propertyPanel #lineWidth").value = parms.lineWidth;
+  panel.querySelector("#propertyPanel #leftEdge").value = parms.leftEdgeType;
+  panel.querySelector("#propertyPanel #rightEdge").value = parms.rightEdgeType;
+  panel.querySelector("#propertyPanel #lineType").value = parms.lineType;
+  panel.querySelector("#propertyPanel #color").value = parms.color;
+
+}
+
+
+module.exports.show = show;
+module.exports.update = update;
